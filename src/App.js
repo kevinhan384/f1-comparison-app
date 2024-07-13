d5116f4e9e98e2b540e5b20a71486a42f22cb2d8
Kevin Han <kevinhan@kevins-air-5.mynetworksettings.com>
2024-07-13 02:00:15 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header'
+import Footer from './Footer'
 
 function App() {
+  const handleClick = () => {
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="content">
+        <p>Content goes here</p>
+        <button onClick={handleClick}>Compare</button>
+      </div>
+      <Footer />
     </div>
   );
 }
